cc203a4191ce9378638f8b96649f6cddf433201f
nick black <dankamongmen@gmail.com>
2024-09-18 09:07:28 -0400

    esp32: define some pins, initialize NVS

--- esp32-s3/dankdryer/dankdryer.ino
+++ esp32-s3/dankdryer/dankdryer.ino
@@ -1,14 +1,30 @@
 // dankdryer firmware
 // intended for use on an ESP32-S3-WROOM-1
 
+#include <nvs.h>
 #include <HX711.h>
+#include <nvs_flash.h>
 #include <driver/temperature_sensor.h>
 
-#define LOAD_DATAPIN 6
-#define LOAD_CLOCKPIN 5
+#define LOWER_PWMPIN 4
+#define UPPER_PWMPIN 5
+#define MOTOR_PWMPIN 35
+#define UPPER_TACHPIN 36
+#define LOWER_TACHPIN 37
+// 38 is used for RGB LED
+#define LOAD_CLOCKPIN 41
+#define LOAD_DATAPIN 42
+#define MOTOR_1PIN 47
+#define MOTOR_2PIN 48
 
 HX711 Load;
+static bool UsePersistentStore; // set true upon successful initialization
 static temperature_sensor_handle_t temp;
+
+// defaults, some of which can be configured.
+static unsigned LowerPWM = 128;
+static unsigned UpperPWM = 128;
+static unsigned TargetTemp = 80;
 
 float getAmbient(void){
   float t;
@@ -34,10 +50,43 @@ int setup_esp32temp(void){
   return 0;
 }
 
+int init_pstore(void){
+  printf("initializing persistent store...\n");
+  esp_err_t err = nvs_flash_init();
+  if(err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND){
+    // FIXME LED feedback during erase + init
+    printf("erasing flash...\n");
+    err = nvs_flash_erase();
+    if(err == ESP_OK){
+      printf("initializing flash...\n");
+      err = nvs_flash_init();
+    }
+  }
+  if(err){
+    // FIXME LED feedback
+    printf("failure (%d) initializing nvs!\n", err);
+    return -1;
+  }
+  return 0;
+}
+
+// read and update boot count, read configurable defaults from pstore if they
+// are present (we do not write defaults to pstore, so we can differentiate
+// between defaults and a configured value).
+int read_pstore(void){
+  return 0;
+}
+
 void setup(void){
+  Serial.begin(115200);
+  if(!init_pstore()){
+    if(!read_pstore()){
+      UsePersistentStore = true;
+    }
+  }
   Load.begin(LOAD_DATAPIN, LOAD_CLOCKPIN);
-  while(setup_esp32temp()){
-    ;
+  if(setup_esp32temp()){
+    // FIXME LED feedback
   }
 }
 
