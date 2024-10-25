ace62c3e5bad87991a7837994c5094a7155b4fdf
nick black <dankamongmen@gmail.com>
2024-10-25 10:20:42 -0400

    probe for NAU7802 with logging

--- esp32-s3/main/nau7802.c
+++ esp32-s3/main/nau7802.c
@@ -1,13 +1,19 @@
 #include "nau7802.h"
+#include <esp_log.h>
+
+static const char* TAG = "nau";
 
 #define NAU7802_ADDRESS 0x2A
 
 int nau7802_detect(i2c_master_bus_handle_t i2c, bool* present){
-  esp_err_t e = i2c_master_probe(i2c, NAU7802_ADDRESS, -1);
+  const unsigned addr = NAU7802_ADDRESS;
+  esp_err_t e = i2c_master_probe(i2c, addr, -1);
   if(e != ESP_OK){
+    ESP_LOGW(TAG, "error %d detecting NAU7802", e);
     *present = false;
     return -1;
   }
+  ESP_LOGI(TAG, "successfully detected NAU7802 at %02x", addr);
   *present = true;
   return 0;
 }
