3830945c311fc10e6dc80832144b0e102e18803a
nick black <dankamongmen@gmail.com>
2024-11-16 16:23:10 -0500

    print diagnostics regarding ota partitions

--- esp32-s3/main/ota.c
+++ esp32-s3/main/ota.c
@@ -1,5 +1,22 @@
 #include "ota.h"
+#include <esp_ota_ops.h>
 
 int ota_init(void){
+  const esp_partition_t *boot = esp_ota_get_boot_partition();
+  printf("OTA boot partition: %s %lu:%lu size %ld type %d:%d\n",
+         boot->label, boot->address, boot->address + boot->size - 1,
+         boot->size, boot->type, boot->subtype);
+  boot = esp_ota_get_running_partition();
+  printf("OTA running partition: %s %lu:%lu size %ld type %d:%d\n",
+         boot->label, boot->address, boot->address + boot->size - 1,
+         boot->size, boot->type, boot->subtype);
+  uint8_t pcount = esp_ota_get_app_partition_count();
+  printf("OTA partition count: %u\n", pcount);
+  if(pcount > 1){
+    boot = esp_ota_get_next_update_partition(boot);
+    printf("OTA next partition: %s %lu:%lu size %ld type %d:%d\n",
+          boot->label, boot->address, boot->address + boot->size - 1,
+          boot->size, boot->type, boot->subtype);
+  }
   return 0;
 }
