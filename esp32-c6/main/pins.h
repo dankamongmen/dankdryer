41d0d6527c15d36fd452255cf9ffc1d3546455a7
nick black <dankamongmen@gmail.com>
2024-12-31 06:18:55 -0500

    get off the strapping pins. hardware version 1.0

--- esp32-c6/main/pins.h
+++ esp32-c6/main/pins.h
@@ -5,16 +5,16 @@
 
 // https://docs.espressif.com/projects/esp-idf/en/stable/esp32c6/api-reference/peripherals/gpio.html
 // 0--6 are connected to ADC1
-#define SSR_GPIN      GPIO_NUM_1   // heater solid state relay
-#define THERM_DATAPIN GPIO_NUM_2   // analog thermometer (ADC1)
+#define SCL_PIN       GPIO_NUM_0   // I2C clock
+#define SDA_PIN       GPIO_NUM_1   // I2C data
+#define HALL_DATAPIN  GPIO_NUM_2   // hall sensor
+#define THERM_DATAPIN GPIO_NUM_3   // analog thermometer (ADC1)
 // 4 and 5 are strapping pins
+#define MOTOR_GATEPIN GPIO_NUM_6   // MOSFET gate for motor
 // 8 and 9 are strapping pins
 // 12--13 are used for JTAG (not strictly needed)
-#define SDA_PIN       GPIO_NUM_14  // I2C data
+#define SSR_GPIN      GPIO_NUM_14  // heater solid state relay
 // 15 is a strapping pin
-#define SCL_PIN       GPIO_NUM_18  // I2C clock
-#define MOTOR_GATEPIN GPIO_NUM_22  // MOSFET gate for motor
-#define HALL_DATAPIN  GPIO_NUM_23  // hall sensor
 // 24--30 are reserved for SPI flash
 
 #endif
