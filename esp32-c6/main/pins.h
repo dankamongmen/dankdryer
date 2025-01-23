ea598b9162b45c8c99dda94d60d483627b9895d8
nick black <dankamongmen@gmail.com>
2025-01-23 04:57:25 -0500

    update pin definition to restore direct fan control #84

--- esp32-c6/main/pins.h
+++ esp32-c6/main/pins.h
@@ -5,16 +5,20 @@
 
 // https://docs.espressif.com/projects/esp-idf/en/stable/esp32c6/api-reference/peripherals/gpio.html
 // 0--6 are connected to ADC1
-#define SCL_PIN       GPIO_NUM_0   // I2C clock
-#define SDA_PIN       GPIO_NUM_1   // I2C data
-#define HALL_DATAPIN  GPIO_NUM_2   // hall sensor
-#define THERM_DATAPIN GPIO_NUM_3   // analog thermometer (ADC1)
+#define SDA_PIN       GPIO_NUM_0   // I2C clock
+#define SCL_PIN       GPIO_NUM_1   // I2C data
 // 4 and 5 are strapping pins
-#define MOTOR_GATEPIN GPIO_NUM_6   // MOSFET gate for motor
+#define UPPER_PWMPIN  GPIO_NUM_4   // upper chamber fan pwm
+#define UPPER_TACHPIN GPIO_NUM_5   // upper chamber fan tach
+#define THERM_DATAPIN GPIO_NUM_6   // analog thermometer (ADC1)
 // 8 and 9 are strapping pins
 // 12--13 are used for JTAG (not strictly needed)
-#define SSR_GPIN      GPIO_NUM_14  // heater solid state relay
+#define MOTOR_GATEPIN GPIO_NUM_12   // MOSFET gate for motor
+#define SSR_GPIN      GPIO_NUM_13  // heater solid state relay
 // 15 is a strapping pin
+#define LOWER_PWMPIN  GPIO_NUM_14  // lower chamber fan pwm
+#define LOWER_TACHPIN GPIO_NUM_15  // lower chamber fan tach
+#define HALL_DATAPIN  GPIO_NUM_19  // hall sensor
 // 24--30 are reserved for SPI flash
 
 #endif
