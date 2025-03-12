f330b0ba7b2a008c3396b446ce2712e87dc090e0
nick black <dankamongmen@gmail.com>
2025-03-11 22:19:21 -0400

    move gpio static inlines to pins.h

--- esp32-c6/main/pins.h
+++ esp32-c6/main/pins.h
@@ -1,12 +1,14 @@
 #ifndef DANKDRYER_PINS
 #define DANKDRYER_PINS
 
-// esp32-c6 mini-1u-h4  pin assignments
+#include <driver/gpio.h>
+
+// esp32-c6 mini-1u-h4 pin assignments
 
 // https://docs.espressif.com/projects/esp-idf/en/stable/esp32c6/api-reference/peripherals/gpio.html
 // 0--6 are connected to ADC1
 #define THERM_DATAPIN GPIO_NUM_0   // analog thermometer (ADC1)
-#define HALL_DATAPIN  GPIO_NUM_1  // hall sensor
+#define HALL_DATAPIN  GPIO_NUM_1   // hall sensor
 // 4 and 5 are strapping pins
 #define SDA_PIN       GPIO_NUM_4   // I2C clock
 #define SCL_PIN       GPIO_NUM_5   // I2C data
@@ -15,15 +17,39 @@
 // 8 and 9 are strapping pins
 // 12--13 are used for JTAG (not strictly needed)
 #define MOTOR_GATEPIN GPIO_NUM_12   // MOSFET gate for motor
-#define SSR_GPIN      GPIO_NUM_13  // heater solid state relay
+#define SSR_GPIN      GPIO_NUM_13   // heater solid state relay
 // 15 is a strapping pin
-#define LOWER_PWMPIN  GPIO_NUM_14  // lower chamber fan pwm
-#define LOWER_TACHPIN GPIO_NUM_15  // lower chamber fan tach
+#define LOWER_PWMPIN  GPIO_NUM_14   // lower chamber fan pwm
+#define LOWER_TACHPIN GPIO_NUM_15   // lower chamber fan tach
+/*
 #define LCD_RST_PIN   GPIO_NUM_18
 #define LCD_SCL_PIN   GPIO_NUM_19
 #define LCD_SDA_PIN   GPIO_NUM_20
 #define LCD_DC_PIN    GPIO_NUM_21
 #define LCD_CS_PIN    GPIO_NUM_22
+*/
 // 24--30 are reserved for SPI flash
+
+int gpio_setup(gpio_num_t pin, gpio_mode_t mode, const char *mstr);
+
+static inline int
+gpio_set_inputoutput_opendrain(gpio_num_t pin){
+  return gpio_setup(pin, GPIO_MODE_INPUT_OUTPUT_OD, "input+output(od)");
+}
+
+static inline int
+gpio_set_inputoutput(gpio_num_t pin){
+  return gpio_setup(pin, GPIO_MODE_INPUT_OUTPUT, "input+output");
+}
+
+static inline int
+gpio_set_input(gpio_num_t pin){
+  return gpio_setup(pin, GPIO_MODE_INPUT, "input");
+}
+
+static inline int
+gpio_set_output(gpio_num_t pin){
+  return gpio_setup(pin, GPIO_MODE_OUTPUT, "output");
+}
 
 #endif
