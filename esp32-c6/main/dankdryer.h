e92d746a6eca51f1528b93538ab94ff3c8879b6d
nick black <dankamongmen@gmail.com>
2025-04-07 13:33:01 -0400

    install an interrupt handler on the button pin #69

--- esp32-c6/main/dankdryer.h
+++ esp32-c6/main/dankdryer.h
@@ -4,6 +4,7 @@
 #define NVS_HANDLE_NAME "pstore"
 
 #include <nvs.h>
+#include <driver/gpio.h>
 
 int setup_intr(gpio_num_t pin, _Atomic(uint32_t)* arg);
 int nvs_get_opt_u32(nvs_handle_t nh, const char* recname, uint32_t* val);
--- esp32-c6/main/reset.c
+++ esp32-c6/main/reset.c
@@ -1,11 +1,42 @@
+#include "dankdryer.h"
 #include "reset.h"
+#include <esp_log.h>
 #include <stdatomic.h>
+#include <driver/gpio.h>
+
+#define TAG "freset"
 
 #define FRESET_HOLD_TIME_US 5000000ul
 static _Atomic(int64_t) last_low_start = -1;
 
+static void
+freset_intr(void* v){
+  ESP_LOGI(TAG, "got an interrupt");
+}
+
 int setup_factory_reset(gpio_num_t pin){
-  // FIXME
+  esp_err_t e;
+  if((e = gpio_pullup_dis(pin)) != ESP_OK){
+    fprintf(stderr, "error (%s) disabling pullup on %d\n", esp_err_to_name(e), pin);
+    return -1;
+  }
+  if((e = gpio_pulldown_en(pin)) != ESP_OK){
+    fprintf(stderr, "error (%s) enabling pulldown on %d\n", esp_err_to_name(e), pin);
+    return -1;
+  }
+  if((e = gpio_set_intr_type(pin, GPIO_INTR_ANYEDGE)) != ESP_OK){
+    fprintf(stderr, "failure (%s) installing %d interrupt\n", esp_err_to_name(e), pin);
+    return -1;
+  }
+  if((e = gpio_isr_handler_add(pin, freset_intr, NULL)) != ESP_OK){
+    fprintf(stderr, "failure (%s) setting %d isr\n", esp_err_to_name(e), pin);
+    return -1;
+  }
+  if((e = gpio_intr_enable(pin)) != ESP_OK){
+    fprintf(stderr, "failure (%s) enabling %d interrupt\n", esp_err_to_name(e), pin);
+    return -1;
+  }
+  return 0;
   return 0;
 }
 
